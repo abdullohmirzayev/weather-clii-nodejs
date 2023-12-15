87a4b1fb066365dbdd95940b871d02b993f022e1
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-15 18:45:29 +0500

    added #3 Xabarni chop etish

--- helpers/args.js
+++ helpers/args.js
@@ -15,5 +15,4 @@ const getArgs = (args) => {
   });
   return res;
 };
-
-module.exports = getArgs;
+export default getArgs;
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const getArgs = require("./helpers/args");
+import getArgs from "./helpers/args.js";
+import {
+  printError,
+  printSuccess,
+  printHelp,
+} from "./services/log.services.js";
 
 const startCLI = () => {
   const args = getArgs(process.argv);
+  console.log(args);
   if (args.h) {
+    printHelp();
     // help
   }
   if (args.s) {
