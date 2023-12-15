79e2606ccc8c2f82e7584f8dcab755f8c266a72a
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-15 20:41:13 +0500

    added #4 Token saqlash

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import getArgs from "./helpers/args.js";
+import { saveKeyValue } from "./services/srorage.service.js";
 import {
   printError,
   printSuccess,
   printHelp,
 } from "./services/log.services.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token was saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const startCLI = () => {
   const args = getArgs(process.argv);
@@ -16,6 +26,7 @@ const startCLI = () => {
     // save city
   }
   if (args.t) {
+    return saveToken(args.t);
     // save token
   }
   // result
